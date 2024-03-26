82b8296e1d08e12ad42ec05439ea90048e9233e2
Sudo1999 <105590069+Sudo1999@users.noreply.github.com>
2024-03-26 10:59:28 +0100

    ImageController

---
+++ src/main/java/com/image/crop/controller/ImageController.java
@@ -0,0 +1,18 @@
+package com.image.crop.controller;
+
+public class ImageController {
+
+////  ========  Interfaces graphiques de Java  ========  ////
+
+    //  ====  AWT (Abstract Window Toolkit)  ====
+    // (https://jmdoudoux.developpez.com/cours/developpons/java/chap-awt.php)
+
+    //  ====  Swing  ====
+    // (https://baptiste-wicht.developpez.com/tutoriels/java/swing/debutant/)
+    // (https://gfx.developpez.com/tutoriel/java/gui/)
+
+    //  ====  JavaFX  ====
+    // (https://mikarber.developpez.com/tutoriels/java/introduction-javafx/)
+    // (https://xebia.developpez.com/tutoriels/java/concepts-javafx-illustration-exemple/)
+
+}
